bdab0ecad0d9f74af0c9405dadd914ec56225c13
yourlifeno1 <yourlifeno1@gmail.com>
2026-05-03 23:42:09 +0900

    Update 2_L-Sales Arena.py

--- pages/2_L-Sales Arena.py
+++ pages/2_L-Sales Arena.py
@@ -176,10 +176,11 @@ for i, message in enumerate(st.session_state.messages):
 st.write("---")
 
 # 3개의 컬럼을 생성하여 가운데에 마이크 배치
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2, col3 = st.columns([1.5, 1, 1.5])
 with col2:
+    # 🎤 마이크 버튼 중앙 배치
     audio_info = mic_recorder(
-        start_prompt="🎤 응대 시작 (마이크)", 
+        start_prompt="🎤 응대 시작", 
         stop_prompt="🛑 완료", 
         just_once=True, 
         key='sales_mic'

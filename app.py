91d0ebae61ded973857c1adfee683d4ca2306096
yourlifeno1 <yourlifeno1@gmail.com>
2026-05-04 12:48:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -358,8 +358,7 @@ def show_trend_section():
         
 # --- UI 메인 ---
 st.set_page_config(page_title="LG 라이프 큐레이션", layout="wide")
-
-st.markdown("### LG Life Curation")
+st.markdown("## LG Life Curation")
 
 loc = get_geolocation()
 
